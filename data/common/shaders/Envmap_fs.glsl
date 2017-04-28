91dbf7a7b6a8146dd3fb8a84a6b85e1e046e7b26
John <j.x.chapman@gmail.com>
2017-04-28 18:21:49 +0200

    shaders: fix Envmap_fs

--- data/common/shaders/Envmap_fs.glsl
+++ data/common/shaders/Envmap_fs.glsl
@@ -1,7 +1,9 @@
 #include "shaders/def.glsl"
 #include "shaders/Envmap.glsl"
 
-noperspective in vec3 vViewRayW;
+noperspective in vec2 vUv;
+noperspective in vec3 vFrustumRay;
+noperspective in vec3 vFrustumRayW;
 
 #if   defined(ENVMAP_CUBE)
 	uniform samplerCube txEnvmap;
@@ -14,9 +16,9 @@ layout(location=0) out vec3 fResult;
 void main() 
 {
 #if   defined(ENVMAP_CUBE)
-	vec3 ret = textureLod(txEnvmap, normalize(vViewRayW), 0.0).rgb;
+	vec3 ret = textureLod(txEnvmap, normalize(vFrustumRayW), 0.0).rgb;
 #elif defined(ENVMAP_SPHERE)
-	vec3 ret = textureLod(txEnvmap, Envmap_GetSphereUv(normalize(vViewRayW)), 0.0).rgb;
+	vec3 ret = textureLod(txEnvmap, Envmap_GetSphereUv(normalize(vFrustumRayW)), 0.0).rgb;
 #endif
 	fResult = ret;
 }
